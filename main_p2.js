771d595301bb3765be6247a09de472609b451543
Nhin_Thay <jannik.riegel@gmail.com>
2024-03-21 12:53:02 +0100

    Update main_p2.js

--- main_p2.js
+++ main_p2.js
@@ -17,7 +17,45 @@ function calcular(){
         let paciente = pacientes[index];
         
         alert(paciente.toString()); 
-       
     }
-
 }
+
+function imc() {
+    let dni = prompt("Indica el DNI del paciente buscado:");
+    let index = pacientes.findIndex(edificios => edificios.dni === dni);
+    
+    if (index === -1) { // Si no existe
+        alert("Nombre no válido.")
+    } else {
+        let paciente = pacientes[index];
+        
+        alert(`DNI del paciente: ${paciente.dni}\nNombre Completo: ${paciente.nomComplet}\nIMC: ${paciente.indexImc()}\nCategoría de peso: ${paciente.indexCatPes()}`);
+    }
+}
+
+function indexGrasa() {
+    let dni = prompt("Indica el DNI del paciente buscado:");
+    let index = pacientes.findIndex(edificios => edificios.dni === dni);
+    
+    if (index === -1) { // Si no existe
+        alert("Nombre no válido.")
+    } else {
+        let paciente = pacientes[index];
+        
+        alert(`DNI del paciente: ${paciente.dni}\nNombre Completo: ${paciente.nomComplet}\nÍndice de Grasa: ${paciente.indexGrasa()}`);
+    }
+}
+  
+function rcc() {
+    let dni = prompt("Indica el DNI del paciente buscado:");
+    let index = pacientes.findIndex(edificios => edificios.dni === dni);
+    
+    if (index === -1) { // Si no existe
+        alert("Nombre no válido.")
+    } else {
+        let paciente = pacientes[index];
+        
+        alert(`DNI del paciente: ${paciente.dni}\nNombre Completo: ${paciente.nomComplet}\nRelación Cintura-Cadera: ${paciente.relacionCC()}`);
+    }
+}
+    
